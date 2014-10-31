9bd1faaef134bd5cf9ec9c6422d380cc13e32b75
cjshort <callumshorty@hotmail.com>
2014-10-31 13:30:30 +0000

    messaging system styling and profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :internal
+  # before_action :internal, only: [:home]
 
   def internal # Feeds variables to the 'internal' layout
-   @unread_message_count = Message.where(recipient_id: current_user.id, read_at: nil).count 
+   # @unread_message_count = Message.where(recipient_id: current_user.id, read_at: nil).count 
 
   end
 
